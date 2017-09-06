c2627b3f96737973379eb1f54506ff57dcbde5ac
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2017-09-06 18:27:03 +0200

    Resampling of vectorfields.

--- src/framework/filters/resample.cpp
+++ src/framework/filters/resample.cpp
@@ -2,6 +2,7 @@
 
 #include "debug/assert.h"
 #include "filters/gaussian_filter.h"
+#include "math/float3.h"
 #include "volume/volume_helper.h"
 
 #include <algorithm>
@@ -72,3 +73,77 @@ Volume filters::downsample_volume_gaussian(const Volume& vol, float scale)
     return Volume();
 }
 
+
+Volume filters::downsample_vectorfield(const Volume& vol, float scale, Volume& residual)
+{
+    assert(scale > 0.0f && scale < 1.0f);
+    assert(vol.voxel_type() == voxel::Type_Float3);
+    if (vol.voxel_type() == voxel::Type_Float3)
+    {
+        VolumeHelper<float3> field(vol);
+        float inv_scale = 1.0f / scale;
+
+        Dims old_dims = field.size();
+        Dims new_dims{
+            uint32_t(ceil(old_dims.width * scale)),
+            uint32_t(ceil(old_dims.height * scale)),
+            uint32_t(ceil(old_dims.depth * scale))
+        };
+
+        VolumeHelper<float3> result(new_dims);
+        result.set_origin(field.origin());
+
+        float3 old_spacing = field.spacing();
+        float3 new_spacing{
+            old_spacing.x * inv_scale,
+            old_spacing.y * inv_scale,
+            old_spacing.z * inv_scale
+        };
+        result.set_spacing(new_spacing);
+
+        #pragma omp parallel for
+        for (int z = 0; z < int(new_dims.depth); ++z)
+        {
+            for (int y = 0; y < int(new_dims.height); ++y)
+            {
+                for (int x = 0; x < int(new_dims.width); ++x)
+                {
+                    int px = int(x * inv_scale);
+                    int py = int(y * inv_scale);
+                    int pz = int(z * inv_scale);
+
+                    float3 v = field(px, py, pz);
+
+                    v = v + field.at(px+1, py, pz, volume::Border_Replicate);
+                    v = v + field.at(px, py+1, pz, volume::Border_Replicate);
+                    v = v + field.at(px, py, pz+1, volume::Border_Replicate);
+                    v = v + field.at(px+1, py+1, pz, volume::Border_Replicate);
+                    v = v + field.at(px+1, py, pz+1, volume::Border_Replicate);
+                    v = v + field.at(px, py+1, pz+1, volume::Border_Replicate);
+                    v = v + field.at(px+1, py+1, pz+1, volume::Border_Replicate);
+                    
+                    float s = scale / 8.0f;
+                    result(x, y, z) = float3{s*v.x, s*v.y, s*v.z};
+                }
+            }
+        }
+        
+	    VolumeHelper<float3> tmp(field.size());
+    
+        #pragma omp parallel for
+        for (int z = 0; z < int(old_dims.depth); ++z)
+        {
+            for (int y = 0; y < int(old_dims.height); ++y)
+            {
+                for (int x = 0; x < int(old_dims.width); ++x)
+                {
+                    tmp(x, y, z) = field(x, y, z) - 
+                        inv_scale * result.linear_at(scale*x, scale*y, scale*z, volume::Border_Replicate);
+                }
+            }
+        }
+        residual = tmp;
+    
+    }
+    return Volume();
+}
--- src/framework/filters/resample.h
+++ src/framework/filters/resample.h
@@ -6,4 +6,7 @@ namespace filters
 {
     /// Applies a gaussian prefilter and downsamples the volume by the given scale factor.
     Volume downsample_volume_gaussian(const Volume& vol, float scale);
+
+    /// Downsamples a 3d vectorfield and keeps the residual
+    Volume downsample_vectorfield(const Volume& vol, float scale, Volume& residual);
 }
