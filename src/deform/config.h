f587eeb0b56053a883f0865f053de74391ff9ecd
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2017-09-26 16:25:46 +0200

    Debug output only in debug build.

--- src/deform/config.h
+++ src/deform/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+#ifdef DF_BUILD_DEBUG
 // 0 : No debug
 // 3 : 
 // 4 : Spam
@@ -9,6 +9,11 @@
 
 #if DF_DEBUG_LEVEL >= 3
     #define DF_OUTPUT_DEBUG_VOLUMES
+#endif 
 
-#endif // DF_DEBUG_LEVEL >= 3
+#if DF_DEBUG_LEVEL >= 4
+    #define DF_DEBUG_BLOCK_CHANGE_COUNT
+    #define DF_DEBUG_VOXEL_CHANGE_COUNT
+#endif
 
+#endif // DF_BUILD_DEBUG
