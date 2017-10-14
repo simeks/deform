daa2c4610f67eff70ba1de009192de10187cc158
Simon Ekström <simon.ekstrom@gmail.com>
2017-10-14 10:48:28 +0200

    Cost functions for soft constraints and fat-water specific regularization

--- src/deform/cost_function.h
+++ src/deform/cost_function.h
@@ -37,6 +37,41 @@ struct Regularizer
     float3 _spacing;
 };
 
+struct FatWaterRegularizer
+{
+    FatWaterRegularizer(const VolumeHelper<float>& fixed_water) : 
+        _spacing(fixed_water.spacing()), _fixed_water(fixed_water) {}
+
+    /// p   : Position in fixed image
+    /// def0 : Deformation in active voxel [voxels] (in fixed image space)
+    /// def1 : Deformation in neighbor [voxels] (in fixed image space)
+    /// step : Direction to neighbor [voxels]
+    inline float operator()(const int3& p, const float3& def0, const float3& def1, const int3& step)
+    {
+        float3 step_in_mm {step.x*_spacing.x, step.y*_spacing.y, step.z*_spacing.z};
+        
+        float3 diff = def0 - def1;
+        float3 diff_in_mm {diff.x*_spacing.x, diff.y*_spacing.y, diff.z*_spacing.z};
+        
+        float dist_squared = math::length_squared(diff_in_mm);
+        float step_squared = math::length_squared(step_in_mm);
+
+        // Fat = 0.08
+        // Water = 0.1
+
+        // w = 0.08 + 0.02 + avg(water, water+step)
+
+        float water1 = fixed_water(p); // Water should be 0-1
+        float water2 = fixed_water(p+step);
+
+        float w = 0.08f + 0.02f * 0.5f * (water1+water2)
+        return w * dist_squared / step_squared;
+    }
+
+    float3 _spacing;
+    VolumeHelper<float> _fixed_water;
+};
+
 template<typename T>
 struct SquaredDistanceFunction
 {
@@ -80,6 +115,7 @@ struct ConstraintsFunction
     {
         if (_constraints_mask(p) != 0)
         {
+            // Distance in voxels
             float dist = math::length_squared(def - _constraints_values(p));
             if (dist <= 0.0001f)
                 return 0.0f;
@@ -91,6 +127,36 @@ struct ConstraintsFunction
     VolumeUInt8 _constraints_mask;
     VolumeFloat3 _constraints_values;
 };
+
+struct SoftConstraintsFunction
+{
+    SoftConstraintsFunction(const VolumeUInt8& constraint_mask,
+                            const VolumeFloat3& constraints_values) :
+        _constraints_mask(constraint_mask),
+        _constraints_values(constraints_values),
+        _spacing(_constraints_values.spacing())
+    {}
+
+    inline float operator()(const int3& p, const float3& def)
+    {
+        if (_constraints_mask(p) != 0)
+        {
+            float3 diff = def - _constraints_values(p);
+            float3 diff_in_mm {diff.x*_spacing.x, diff.y*_spacing.y, diff.z*_spacing.z};
+            
+            // Distance^2 in [mm]
+            float dist_squared = math::length_squared(diff_in_mm);
+            
+            // y=0.05x^2
+            return std::min(0.05f*dist_squared, 1000.0f); // Clamp to avoid explosion
+        }
+        return 0.0f;
+    }
+    VolumeUInt8 _constraints_mask;
+    VolumeFloat3 _constraints_values;
+    float3 _spacing;
+};
+
 
 
 template<typename T, size_t I>
