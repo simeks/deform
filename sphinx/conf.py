830b5b5c394028a1f7f67bb0ded61724dccd6757
m-pilia <martino.pilia@gmail.com>
2018-07-14 17:18:27 +0200

    static and template dirs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -58,7 +58,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates'] # do not use leading underscore to not mess with Jekyll
+templates_path = ['@SPHINX_TEMPLATES@'] # do not use leading underscore to not mess with Jekyll
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -101,7 +101,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static'] # do not use leading underscore to not mess with Jekyll
+html_static_path = ['@SPHINX_STATIC@'] # do not use leading underscore to not mess with Jekyll
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
