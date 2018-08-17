57441a5c9cbaebbfa754e496501c210fd8e4320e
Simon Ekström <simon.ekstrom@gmail.com>
2018-08-17 12:55:55 +0200

    GPU Kernel for NCC

--- src/deform_lib/registration/gpu/cost_function.cu
+++ src/deform_lib/registration/gpu/cost_function.cu
@@ -51,6 +51,95 @@ __global__ void ssd_kernel(
     cost_acc(x,y,z) = f*f;
 }
 
+template<typename T>
+__global__ void ncc_kernel(
+    cudaTextureObject_t fixed,
+    cudaTextureObject_t moving,
+    cuda::VolumePtr<float4> df,
+    int radius,
+    dim3 fixed_dims,
+    dim3 moving_dims,
+    float4 fixed_origin,
+    float4 fixed_spacing,
+    float4 moving_origin,
+    float4 moving_spacing,
+    cuda::VolumePtr<float> cost_acc
+)
+{
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int z = blockIdx.z*blockDim.z + threadIdx.z;
+
+    if (x >= fixed_dims.x ||
+        y >= fixed_dims.y ||
+        z >= fixed_dims.z)
+    {
+        return;
+    }
+
+    float4 world_p = fixed_origin + float4{float(x),float(y),float(z),0} * fixed_spacing; 
+    float4 moving_p = (world_p + df(x,y,z) - moving_origin) / moving_spacing; 
+    
+    // [Filip]: Addition for partial-body registrations
+    if (moving_p.x < 0 || moving_p.x > moving_dims.x || 
+        moving_p.y < 0 || moving_p.y > moving_dims.y || 
+        moving_p.z < 0 || moving_p.z > moving_dims.z) {
+        // Does not affect the cost accumulator
+        return;
+    }
+
+    float sff = 0.0;
+    float smm = 0.0;
+    float sfm = 0.0;
+    float sf = 0.0;
+    float sm = 0.0;
+    unsigned int n = 0;
+
+    for (int dz = -radius; dz <= radius; ++dz) {
+        for (int dy = -radius; dy <= radius; ++dy) {
+            for (int dx = -radius; dx <= radius; ++dx) {
+                // TODO: Does not account for anisotropic volumes
+                int r2 = dx*dx + dy*dy + dz*dz;
+                if (r2 > radius * radius)
+                    continue;
+
+                float3 fp{float(x + dx), float(y + dy), float(z + dz)};
+                
+                // if (!stk::is_inside(_fixed.size(), fp))
+                //     continue;
+
+                float3 mp{moving_p.x + dx, moving_p.y + dy, moving_p.z + dz};
+
+                T fixed_v = tex3D<T>(fixed, fp.x+0.5f, fp.y+0.5f, fp.z+0.5f);
+                T moving_v = tex3D<T>(moving, mp.x+0.5f, mp.y+0.5f, mp.z+0.5f);
+
+                sff += fixed_v * fixed_v;
+                smm += moving_v * moving_v;
+                sfm += fixed_v*moving_v;
+                sm += moving_v;
+                sf += fixed_v;
+
+                ++n;
+            }
+        }
+    }
+
+    if (n == 0)
+        return;
+
+    // Subtract mean
+    sff -= (sf * sf / n);
+    smm -= (sm * sm / n);
+    sfm -= (sf * sm / n);
+    
+    float d = sqrt(sff*smm);
+
+    if(d > 1e-14) {
+        cost_acc(x,y,z) = 0.5f*(1.0f-float(sfm / d));
+    }
+}
+
+
 void gpu::run_ssd_kernel(
     const stk::GpuVolume& fixed,
     const stk::GpuVolume& moving,
@@ -85,6 +174,55 @@ void gpu::run_ssd_kernel(
         cuda::TextureObject(fixed, tex_desc),
         cuda::TextureObject(moving, tex_desc),
         df,
+        dims,
+        moving.size(),
+        float4{fixed.origin().x, fixed.origin().y, fixed.origin().z, 0},
+        float4{fixed.spacing().x, fixed.spacing().y, fixed.spacing().z, 1},
+        float4{moving.origin().x, moving.origin().y, moving.origin().z, 0},
+        float4{moving.spacing().x, moving.spacing().y, moving.spacing().z, 1},
+        cost_acc
+    );
+
+    CUDA_CHECK_ERRORS(cudaPeekAtLastError());
+    CUDA_CHECK_ERRORS(cudaDeviceSynchronize());
+}
+
+void gpu::run_ncc_kernel(
+    const stk::GpuVolume& fixed,
+    const stk::GpuVolume& moving,
+    const stk::GpuVolume& df,
+    int radius,
+    stk::GpuVolume& cost_acc
+)
+{
+    ASSERT(fixed.usage() == stk::gpu::Usage_Texture);
+    ASSERT(moving.usage() == stk::gpu::Usage_Texture);
+    ASSERT(df.usage() == stk::gpu::Usage_PitchedPointer);
+
+    FATAL_IF(fixed.voxel_type() != stk::Type_Float || moving.voxel_type() != stk::Type_Float)
+        << "Unsupported format";
+
+    cudaTextureDesc tex_desc;
+    memset(&tex_desc, 0, sizeof(tex_desc));
+    tex_desc.addressMode[0] = cudaAddressModeBorder;
+    tex_desc.addressMode[1] = cudaAddressModeBorder;
+    tex_desc.addressMode[2] = cudaAddressModeBorder;
+    tex_desc.filterMode = cudaFilterModeLinear;
+
+    dim3 dims = cost_acc.size();
+
+    dim3 block_size{32,32,1};
+    dim3 grid_size {
+        (dims.x + block_size.x - 1) / block_size.x,
+        (dims.y + block_size.y - 1) / block_size.y,
+        (dims.z + block_size.z - 1) / block_size.z
+    };
+
+    ncc_kernel<float><<<grid_size, block_size>>>(
+        cuda::TextureObject(fixed, tex_desc),
+        cuda::TextureObject(moving, tex_desc),
+        df,
+        radius,
         dims,
         moving.size(),
         float4{fixed.origin().x, fixed.origin().y, fixed.origin().z, 0},
--- src/deform_lib/registration/gpu/cost_function.h
+++ src/deform_lib/registration/gpu/cost_function.h
@@ -11,4 +11,11 @@ namespace gpu {
         const stk::GpuVolume& df,
         stk::GpuVolume& cost_acc
     );
+    void run_ncc_kernel(
+        const stk::GpuVolume& fixed,
+        const stk::GpuVolume& moving,
+        const stk::GpuVolume& df,
+        int radius,
+        stk::GpuVolume& cost_acc
+    );
 }
