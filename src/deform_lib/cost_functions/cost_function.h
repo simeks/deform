1a274cab2008ee27e97b4042fb3f959aae7de152
Martino Pilia <martino.pilia@gmail.com>
2018-09-04 19:22:46 +0200

    gradient ssd

--- src/deform_lib/cost_functions/cost_function.h
+++ src/deform_lib/cost_functions/cost_function.h
@@ -5,6 +5,7 @@
 #include "squared_distance.h"
 #include "cross_correlation_sphere.h"
 #include "cross_correlation_cube.h"
+#include "gradient_squared_distance.h"
 #include "landmarks.h"
 #include "mutual_information.h"
 #include "sub_function.h"
