4d5abcbbbeb5c70722f3197e78b4038ade312ef3
Simon <simon.ekstrom@gmail.com>
2018-10-20 16:26:38 +0200

    Refactor NCC, making it multi-pass

--- src/deform_lib/registration/gpu/cost_functions/cross_correlation.cu
+++ src/deform_lib/registration/gpu/cost_functions/cross_correlation.cu
@@ -4,28 +4,196 @@
 
 namespace cuda = stk::cuda;
 
-template<typename T, bool use_fixed_mask, bool use_moving_mask>
-__global__ void ncc_kernel(
-    cuda::VolumePtr<T> fixed,
-    cuda::VolumePtr<T> moving,
-    const cuda::VolumePtr<float> fixed_mask,
-    const cuda::VolumePtr<float> moving_mask,
-    cuda::VolumePtr<float4> df,
-    float3 delta,
-    float weight,
-    int radius,
-    int3 offset,
+template<typename T, typename TImpl, bool use_fixed_mask=false, bool use_moving_mask=false>
+struct CostFunctionKernel
+{
+    CostFunctionKernel(
+        const TImpl& impl,
+        const stk::GpuVolume& fixed,
+        const stk::GpuVolume& moving,
+        const stk::GpuVolume& fixed_mask,
+        const stk::GpuVolume& moving_mask,
+        const stk::GpuVolume& df,
+        float weight,
+        stk::GpuVolume& cost // float2
+    ) :
+        _impl(impl),
+        _fixed(fixed),
+        _moving(moving),
+        _fixed_dims(fixed.size()),
+        _moving_dims(moving.size()),
+        _fixed_mask(fixed_mask),
+        _moving_mask(moving_mask),
+        _df(df),
+        _fixed_origin(fixed.origin()),
+        _fixed_spacing(fixed.spacing()),
+        _fixed_direction(fixed.direction()),
+        _moving_origin(moving.origin()),
+        _inv_moving_spacing({1.0f / moving.spacing().x, 1.0f / moving.spacing().y, 1.0f / moving.spacing().z}),
+        _inv_moving_direction(moving.inverse_direction()),
+        _weight(weight),
+        _cost(cost)
+    {
+        ASSERT(cost.voxel_type() == stk::Type_Float2);
+    }
+    ~CostFunctionKernel()
+    {
+    }
+
+    // x,y,z        : Global image coordinates
+    // cost_offset  :  Where in `cost` to store the results (0 or 1).
+    __device__ void operator()(int x, int y, int z, float3 delta, int cost_offset)
+    {
+        // Check if the fixed voxel is masked out
+        if (use_fixed_mask) {
+            if (_fixed_mask(x, y, z) <= FLT_EPSILON) {
+                return;
+            }
+        }
+
+        const float3 d0 { _df(x,y,z).x + delta.x, _df(x, y, z).y + delta.y, _df(x, y, z).z + delta.z };
+
+        const float3 xyz = float3{float(x),float(y),float(z)};
+        const float3 world_p = _fixed_origin + _fixed_direction * (xyz * _fixed_spacing);
+
+        const float3 moving_p = (_inv_moving_direction * (world_p + d0 - _moving_origin)) * _inv_moving_spacing;
+
+        // Check if the moving voxels are masked out
+        float mask_value = 1.0f;
+        if (use_moving_mask) {
+            mask_value = cuda::linear_at_border<float>(
+                    _moving_mask, _moving_dims, moving_p.x, moving_p.y, moving_p.z);
+
+            if (mask_value < FLT_EPSILON) {
+                return;
+            }
+        }
+        
+        float c = _impl(
+            _fixed,
+            _fixed_dims,
+            _moving,
+            _moving_dims,
+            int3{x,y,z},
+            moving_p
+        );
+
+        reinterpret_cast<float*>(&_cost(x,y,z))[cost_offset] += _weight * mask_value * c; 
+    }
+
+    TImpl _impl;
+
+    cuda::VolumePtr<T> _fixed;
+    cuda::VolumePtr<T> _moving;
+    dim3 _fixed_dims;
+    dim3 _moving_dims;
+
+    cuda::VolumePtr<float> _fixed_mask;
+    cuda::VolumePtr<float> _moving_mask;
+    cuda::VolumePtr<float4> _df;
+
+    float3 _fixed_origin;
+    float3 _fixed_spacing;
+    Matrix3x3f _fixed_direction;
+
+    float3 _moving_origin;
+    float3 _inv_moving_spacing;
+    Matrix3x3f _inv_moving_direction;
+
+    float _weight;
+
+    cuda::VolumePtr<float2> _cost;
+};
+
+template<typename T>
+struct NCCImpl
+{
+    NCCImpl(int radius) : _radius(radius) {}
+
+    __device__ float operator()(
+        const cuda::VolumePtr<T>& fixed,
+        const dim3& fixed_dims,
+        cuda::VolumePtr<T> moving,
+        const dim3& moving_dims,
+        const int3& fixed_p, // Reference space
+        const float3& moving_p // Moving space
+    )
+    {
+        float sff = 0.0f;
+        float sf = 0.0f;
+
+        float smm = 0.0f;
+        float sfm = 0.0f;
+        float sm = 0.0f;
+
+        unsigned int n = 0;
+
+        for (int dz = -_radius; dz <= _radius; ++dz) {
+            for (int dy = -_radius; dy <= _radius; ++dy) {
+                for (int dx = -_radius; dx <= _radius; ++dx) {
+                    // TODO: Does not account for anisotropic volumes
+                    int r2 = dx*dx + dy*dy + dz*dz;
+                    if (r2 > _radius * _radius)
+                        continue;
+
+                    int3 fp{fixed_p.x + dx, fixed_p.y + dy, fixed_p.z + dz};
+
+                    if (fp.x < 0 || fp.x >= int(fixed_dims.x) ||
+                        fp.y < 0 || fp.y >= int(fixed_dims.y) ||
+                        fp.z < 0 || fp.z >= int(fixed_dims.z))
+                        continue;
+
+                    float3 mp{moving_p.x + dx, moving_p.y + dy, moving_p.z + dz};
+
+                    float fixed_v = fixed(fp.x, fp.y, fp.z);
+
+                    float moving_v = cuda::linear_at_border<float>(moving, moving_dims, mp.x, mp.y, mp.z);
+
+                    sff += fixed_v * fixed_v;
+
+                    smm += moving_v * moving_v;
+                    sfm += fixed_v*moving_v;
+                    sm += moving_v;
+
+                    sf += fixed_v;
+
+                    ++n;
+                }
+            }
+        }
+
+        if (n == 0)
+            return 0;
+
+        // Subtract mean
+        sff -= (sf * sf / n);
+        smm -= (sm * sm / n);
+        sfm -= (sf * sm / n);
+
+        float denom = sqrt(sff*smm);
+
+        // Set cost to zero if outside moving volume
+
+        if (moving_p.x >= 0 && moving_p.x < moving_dims.x &&
+            moving_p.y >= 0 && moving_p.y < moving_dims.y &&
+            moving_p.z >= 0 && moving_p.z < moving_dims.z &&
+            denom > 1e-5)
+        {
+            return 0.5f * (1.0f-float(sfm / denom));
+        }
+        return 0;
+    }
+
+    int _radius;
+};
+
+template<typename TKernel>
+__global__ void run_cost_kernel(
+    TKernel kernel, 
+    int3 offset, 
     int3 dims,
-    dim3 fixed_dims,
-    dim3 moving_dims,
-    float3 fixed_origin,
-    float3 fixed_spacing,
-    Matrix3x3f fixed_direction,
-    float3 moving_origin,
-    float3 inv_moving_spacing,
-    Matrix3x3f inv_moving_direction,
-    cuda::VolumePtr<float2> cost_acc
-)
+    float3 delta,
+    int cost_offset)
 {
     int x = blockIdx.x*blockDim.x + threadIdx.x;
     int y = blockIdx.y*blockDim.y + threadIdx.y;
@@ -42,119 +210,7 @@ __global__ void ncc_kernel(
     y += offset.y;
     z += offset.z;
 
-    // Check if the fixed voxel is masked out
-    if (use_fixed_mask) {
-        if (fixed_mask(x, y, z) <= FLT_EPSILON) {
-            return;
-        }
-    }
-
-    const float3 d0 { df(x,y,z).x, df(x, y, z).y, df(x, y, z).z };
-    const float3 d1 = d0 + delta;
-
-    const float3 xyz = float3{float(x),float(y),float(z)};
-    const float3 world_p = fixed_origin + fixed_direction * (xyz * fixed_spacing);
-
-    const float3 moving_p0 = (inv_moving_direction * (world_p + d0 - moving_origin)) * inv_moving_spacing;
-    const float3 moving_p1 = (inv_moving_direction * (world_p + d1 - moving_origin)) * inv_moving_spacing;
-
-    // Check if the moving voxels are masked out
-    float mask_value_0 = 1.0f;
-    float mask_value_1 = 1.0f;
-    if (use_moving_mask) {
-        mask_value_0 = cuda::linear_at_border<float>(
-                moving_mask, moving_dims, moving_p0.x, moving_p0.y, moving_p0.z);
-        mask_value_1 = cuda::linear_at_border<float>(
-                moving_mask, moving_dims, moving_p1.x, moving_p1.y, moving_p1.z);
-
-        if (mask_value_0 < FLT_EPSILON && mask_value_1 < FLT_EPSILON) {
-            return;
-        }
-    }
-
-    float sff = 0.0f;
-    float sf = 0.0f;
-
-    float smm0 = 0.0f;
-    float smm1 = 0.0f;
-    float sfm0 = 0.0f;
-    float sfm1 = 0.0f;
-    float sm0 = 0.0f;
-    float sm1 = 0.0f;
-
-    unsigned int n = 0;
-
-    for (int dz = -radius; dz <= radius; ++dz) {
-        for (int dy = -radius; dy <= radius; ++dy) {
-            for (int dx = -radius; dx <= radius; ++dx) {
-                // TODO: Does not account for anisotropic volumes
-                int r2 = dx*dx + dy*dy + dz*dz;
-                if (r2 > radius * radius)
-                    continue;
-
-                int3 fp{x + dx, y + dy, z + dz};
-
-                if (fp.x < 0 || fp.x >= int(fixed_dims.x) ||
-                    fp.y < 0 || fp.y >= int(fixed_dims.y) ||
-                    fp.z < 0 || fp.z >= int(fixed_dims.z))
-                    continue;
-
-                float3 mp0{moving_p0.x + dx, moving_p0.y + dy, moving_p0.z + dz};
-                float3 mp1{moving_p1.x + dx, moving_p1.y + dy, moving_p1.z + dz};
-
-                float fixed_v = fixed(fp.x, fp.y, fp.z);
-
-                float moving_v0 = cuda::linear_at_border<float>(moving, moving_dims, mp0.x, mp0.y, mp0.z);
-                float moving_v1 = cuda::linear_at_border<float>(moving, moving_dims, mp1.x, mp1.y, mp1.z);
-
-                sff += fixed_v * fixed_v;
-
-                smm0 += moving_v0 * moving_v0;
-                smm1 += moving_v1 * moving_v1;
-
-                sfm0 += fixed_v*moving_v0;
-                sfm1 += fixed_v*moving_v1;
-
-                sm0 += moving_v0;
-                sm1 += moving_v1;
-
-                sf += fixed_v;
-
-                ++n;
-            }
-        }
-    }
-
-    if (n == 0)
-        return;
-
-    // Subtract mean
-    sff -= (sf * sf / n);
-    smm0 -= (sm0 * sm0 / n);
-    sfm0 -= (sf * sm0 / n);
-    smm1 -= (sm1 * sm1 / n);
-    sfm1 -= (sf * sm1 / n);
-
-    float denom1 = sqrt(sff*smm1);
-    float denom0 = sqrt(sff*smm0);
-
-    // Set cost to zero if outside moving volume
-
-    if (moving_p0.x >= 0 && moving_p0.x < moving_dims.x &&
-        moving_p0.y >= 0 && moving_p0.y < moving_dims.y &&
-        moving_p0.z >= 0 && moving_p0.z < moving_dims.z &&
-        denom0 > 1e-5 && mask_value_0 >= FLT_EPSILON)
-    {
-        cost_acc(x,y,z).x += mask_value_0 * weight * 0.5f * (1.0f-float(sfm0 / denom0));
-    }
-
-    if (moving_p1.x >= 0 && moving_p1.x < moving_dims.x &&
-        moving_p1.y >= 0 && moving_p1.y < moving_dims.y &&
-        moving_p1.z >= 0 && moving_p1.z < moving_dims.z &&
-        denom1 > 1e-5 && mask_value_1 >= FLT_EPSILON)
-    {
-        cost_acc(x,y,z).y += mask_value_1 * weight * 0.5f * (1.0f-float(sfm1 / denom1));
-    }
+    kernel(x, y, z, delta, cost_offset);
 }
 
 void GpuCostFunction_NCC::cost(
@@ -198,45 +254,53 @@ void GpuCostFunction_NCC::cost(
         1.0f / _moving.spacing().z
     };
 
-    auto (*kernel) = &ncc_kernel<float, false, false>;
-    if (_fixed_mask.valid()) {
-        if (_moving_mask.valid()) {
-            kernel = &ncc_kernel<float, true, true>;
-        }
-        else {
-            kernel = &ncc_kernel<float, true, false>;
-        }
-    }
-    else {
-        if (_moving_mask.valid()) {
-            kernel = &ncc_kernel<float, false, true>;
-        }
-        else {
-            kernel = &ncc_kernel<float, false, false>;
-        }
-    }
+    // auto (*kernel) = &ncc_kernel<float, false, false>;
+    // if (_fixed_mask.valid()) {
+    //     if (_moving_mask.valid()) {
+    //         kernel = &ncc_kernel<float, true, true>;
+    //     }
+    //     else {
+    //         kernel = &ncc_kernel<float, true, false>;
+    //     }
+    // }
+    // else {
+    //     if (_moving_mask.valid()) {
+    //         kernel = &ncc_kernel<float, false, true>;
+    //     }
+    //     else {
+    //         kernel = &ncc_kernel<float, false, false>;
+    //     }
+    // }
 
-    kernel<<<grid_size, block_size, 0, stream>>>(
+    auto kernel = CostFunctionKernel<float,  NCCImpl<float>>(
+        NCCImpl<float>(2),
         _fixed,
         _moving,
         _fixed_mask,
         _moving_mask,
         df,
-        delta,
         weight,
-        _radius,
-        offset,
-        dims,
-        _fixed.size(),
-        _moving.size(),
-        _fixed.origin(),
-        _fixed.spacing(),
-        _fixed.direction(),
-        _moving.origin(),
-        inv_moving_spacing,
-        _moving.inverse_direction(),
         cost_acc
     );
+
+    // E(u(x))
+    run_cost_kernel<<<grid_size, block_size, 0, stream>>>(
+        kernel,
+        offset,
+        dims,
+        float3{0,0,0},
+        0
+    );
+
+    // E(u(x)+d)
+    run_cost_kernel<<<grid_size, block_size, 0, stream>>>(
+        kernel,
+        offset,
+        dims,
+        delta,
+        1
+    );
+
     CUDA_CHECK_ERRORS(cudaPeekAtLastError());
 }
 
