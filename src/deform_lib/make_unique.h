45ff3c3c1187151ed3e817a3491b67a84ff2b288
Simon <simon.ekstrom@gmail.com>
2019-08-16 11:54:29 +0200

    Only implement make_unique when not available

--- src/deform_lib/make_unique.h
+++ src/deform_lib/make_unique.h
@@ -2,8 +2,15 @@
 
 #include <memory>
 
-template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args&& ...args )
-{
-    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
-}
+#if (defined(_MSC_VER) && _MSC_VER < 1800) \
+    || (defined(__GNUC__) && __cplusplus < 201402L)
+
+    template<typename T, typename ...Args>
+    std::unique_ptr<T> make_unique( Args&& ...args )
+    {
+        return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+    }
+
+#else
+    using std::make_unique;
+#endif
