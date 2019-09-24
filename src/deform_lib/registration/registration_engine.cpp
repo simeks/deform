030791e3af25719ddba4defe15b37ea27eb3a980
Simon <simon.ekstrom@gmail.com>
2019-09-24 14:35:45 +0200

    Fixed bug when settings 'regularize_initial_displacement' to true

--- src/deform_lib/registration/registration_engine.cpp
+++ src/deform_lib/registration/registration_engine.cpp
@@ -203,9 +203,13 @@ void RegistrationEngine::build_regularizer(int level, Regularizer& binary_fn)
     binary_fn.set_regularization_scale(_settings.levels[level].regularization_scale);
     binary_fn.set_regularization_exponent(_settings.levels[level].regularization_exponent);
 
+    stk::Volume df = _deformation_pyramid.volume(level);
     if (!_settings.regularize_initial_displacement) {
         // Clone the def, because the current copy will be changed when executing the optimizer
-        binary_fn.set_initial_displacement(_deformation_pyramid.volume(level).clone());
+        binary_fn.set_initial_displacement(df.clone());
+    }
+    else {
+        binary_fn.set_initial_displacement(stk::VolumeFloat3(df.size(), float3{0,0,0}));
     }
 
     #ifdef DF_ENABLE_REGULARIZATION_WEIGHT_MAP
